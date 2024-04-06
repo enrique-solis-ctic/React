f2799c0f7f1f4da720f711dcbf8b2ca9f0b9449e
ElWaje <105492177+ElWaje@users.noreply.github.com>
2024-04-06 21:27:45 +0200

    Delete prestamodefi-project directory

--- prestamodefi-project/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as InicioYAutenticacion } from "./InicioYAutenticacion";
-export { default as GestionDeUsuarios } from "./GestionDeUsuarios";
-export { default as OperacionesDePrestamos } from "./OperacionesDePrestamos";
